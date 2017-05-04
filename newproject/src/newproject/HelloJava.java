f7655792e56bb3403784bcebde8141b0ad04b6a2
skku-0001 <skku-0001@4PC-037>
2017-05-04 19:09:57 +0900

    newcommit

--- newproject/src/newproject/HelloJava.java
+++ newproject/src/newproject/HelloJava.java
@@ -4,7 +4,6 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java!");// TODO Auto-generated method stub
-		System.out.println("한글로 출력");
 	}
 
 }
