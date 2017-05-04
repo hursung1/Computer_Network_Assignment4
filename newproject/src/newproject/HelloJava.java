89223f46c20beb65b60fe5473c7eb1187e6d7fd6
skku-0001 <skku-0001@4PC-037>
2017-05-04 19:08:12 +0900

    First commit Add Hellojava.java

---
+++ newproject/src/newproject/HelloJava.java
@@ -0,0 +1,10 @@
+package newproject;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");// TODO Auto-generated method stub
+		System.out.println("한글로 출력");
+	}
+
+}
